a466be8185c0829645f7a748add906bc11f5a689
Liubin <liubin_9396@163.com>
2025-07-11 17:25:08 +0800

    update

--- llm-gen-text/src/App.vue
+++ llm-gen-text/src/App.vue
@@ -1,59 +1,34 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-const endpoint = '/api/sse';
-const userMessage = ref('');
-const content = ref('');
-const stream = ref(false)
+const prompt = ref('');
 const thinking = ref(false);
+const process = ref('0%')
+const imageUrl = ref('');
 
-const sendMessage = async () => {
-  thinking.value = true
-
-  setTimeout(async () => {
-    if (stream.value) {
-      content.value = "";
-      const evtSource = new EventSource(`${endpoint}?question=${userMessage.value}`);
-      evtSource.addEventListener('message', function (e) {
-        thinking.value = false
-        content.value += e.data
-      })
-      evtSource.addEventListener('end', () => { evtSource.close(); });
-    } else {
-      const response = await fetch(endpoint, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${import.meta.env.VITE_MOONSHOT_API_KEY}`
-        },
-        body: JSON.stringify({
-          model: 'deepseek-chat',
-          messages: [{ role: 'user', content: userMessage.value }],
-          stream: stream.value,
-        })
-      });
-      const data = await response.json();
-      content.value = data.choices[0].message.content;
-    }
-  }, 600)
+async function generateImage() {
+  const endpoint = '/api/sse';
+  const modelName = 'flux-dev';
 }
 </script>
 
 <template>
-  <div class="h-screen bg-neutral-100 flex flex-col gap-4 p-4">
-    <div class="input bg-white rounded p-4 flex gap-4">
-      <input type="text" v-model="userMessage" placeholder="Enter your text"
-        class="w-96 px-4 rounded border-none focus-within:outline" />
-      <button @click="sendMessage" class="bg-indigo-500 text-white px-4 py-2 rounded-lg">Ok</button>
-      <label for="stream" class="flex items-center gap-2">
-        <input type="checkbox" v-model="stream" id="stream" />
-        sse
-      </label>
+  <div class="h-screen container mx-auto flex flex-col gap-4 p-4">
+    <div class="input bg-white rounded p-4 flex justify-center gap-4">
+      <input type="text" v-model="prompt" placeholder="Enter your text to generate image"
+        class="w-96 px-4 rounded-full border-none outline outline-neutral-200 focus-within:outline-black" />
+      <button @click="generateImage"
+        class="bg-black text-white px-4 py-2 rounded-3xl cursor-pointer hover:bg-neutral-800">Generate</button>
     </div>
 
-    <div class="answer bg-white rounded p-4">
-      <h2 class="font-bold text-lg">Answer: </h2>
-      <p v-if="thinking" class="text-sm">思考中...</p>
-      <p class="text-sm">{{ content }}</p>
+    <div class="answer bg-white rounded p-4 flex flex-col gap-3">
+      <h2 class="font-bold text-lg">Image: </h2>
+
+      <div class="text-sm w-96 h-96 bg-neutral-200 flex justify-center items-center rounded-lg p-4">
+        <div v-if="!thinking"
+          class="text-sm w-6 h-6 border border-black rounded-full border-l-transparent border-b-transparent border-r-transparent border-t-neutral-600 animate-spin">
+        </div>
+        <img v-else src="#" alt="image" class="w-full h-full object-cover" />
+      </div>
     </div>
   </div>
 </template>
